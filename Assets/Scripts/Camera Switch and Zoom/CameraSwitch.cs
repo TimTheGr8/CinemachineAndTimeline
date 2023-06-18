14005da71cc9513db0650e3bcd0b29096655fb42
TimTheGr8 <tjanssen4@gmail.com>
2023-06-17 23:04:28 -0500

    Finished Post Processing scene.

--- Assets/Scripts/Camera Switch and Zoom/CameraSwitch.cs
+++ Assets/Scripts/Camera Switch and Zoom/CameraSwitch.cs
@@ -7,14 +7,7 @@ public class CameraSwitch : MonoBehaviour
     [SerializeField]
     private List<GameObject> _vCams = new List<GameObject>();
 
-    [SerializeField]
     private int _currentCamIndex = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -24,16 +17,12 @@ public class CameraSwitch : MonoBehaviour
             _currentCamIndex++;
             if (_currentCamIndex > _vCams.Count - 1)
                 _currentCamIndex = 0;
+
             foreach(var cam in _vCams)
             {
                 cam.SetActive(false);
             }
             _vCams[_currentCamIndex].SetActive(true);
-
-            //for (int i = 0; i < _vCams.Count - 1; i++)
-            //{
-            //    _vCams[i].SetActive(false);
-            //}
         }
     }
 }
