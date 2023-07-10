516d4db1f8f3db3617c7413d3b4a6479cb0608b2
TimTheGr8 <tjanssen4@gmail.com>
2023-07-10 16:29:07 -0500

    Finished the Signal Emitter scene.

---
+++ Assets/Scripts/Signal Emmiters/Sphere_01.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere_01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Sphere01()
+    {
+        Debug.Log("This is Sphere 01 Log.");
+    }
+}
